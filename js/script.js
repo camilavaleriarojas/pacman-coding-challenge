90ff6f2db19d94696d4459152698d253af47f9c1
Camila Rojas <camila.rojas@radiumrocket.com>
2023-08-04 16:14:50 -0300

    Add event listener to get elements by id

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var pacman;
 var ghost;
+var cookie;
 
-window.addEventListener('DOMContentLoaded', function(){
-    pacman = document.getElementById('pacman');
-    ghost = document.getElementById('ghost');
-    console.log(pacman);
-    console.log(ghost);
+window.addEventListener("DOMContentLoaded", function () {
+  pacman = document.getElementById("pacman");
+  ghost = document.getElementById("ghost");
+  cookie = document.getElementById("cookie");
 });
